ceb21e69339cf15d168882575ed83220830b4a9f
903345072 <903345072@qq.com>
2019-08-20 14:08:15 +0800

    312

--- frontend/views/order/productDetail.php
+++ frontend/views/order/productDetail.php
@@ -235,7 +235,7 @@ html{
   function getDataMin30() {
     $.ajax({
 
-         url: 'http://dt.jctytech.com/stock.php?u=wwwzzzzdd0599&symbol='+symbol+'&type=kline&line=min,30&num=100&sort=Date%20desc',
+         url: 'http://dt.jctytech.com/stock.php?u=hongyiqihuo&symbol='+symbol+'&type=kline&line=min,30&num=100&sort=Date%20desc',
         //url: "<?=url('site/get-data')?>" + "?symbol="+symbol+'&type=5',
         async: true,
         dataType:'json',
