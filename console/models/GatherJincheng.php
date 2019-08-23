5e8ad9f5619435e89ec404f377d7bc56e1f57cc4
903345072 <903345072@qq.com>
2019-08-23 19:37:16 +0800

    ads

--- console/models/GatherJincheng.php
+++ console/models/GatherJincheng.php
@@ -146,8 +146,8 @@ class GatherJincheng extends Gather
                                         $_data['price'] = $expect_point;
                                         cache('now_point'.$k,$_data['price'],1800000);
                                         $product->c_state = 'b';     //达到预期点位强制回落到正常点位
-                                        $product->expect_time = time()+300;
-                                        $product->expect_minit = 5;
+                                        $product->expect_time = time()+600;
+                                        $product->expect_minit = 10;
                                         $product->expect_point = $data2[0]['NewPrice'];
                                         $product->save(0);
                                     }
@@ -159,8 +159,8 @@ class GatherJincheng extends Gather
                                         $_data['price'] = $expect_point;
                                         cache('now_point'.$k,$_data['price'],1800000);
                                         $product->c_state = 'a';     //达到预期点位强制上升到正常点位
-                                        $product->expect_time = time()+300;
-                                        $product->expect_minit = 5;
+                                        $product->expect_time = time()+600;
+                                        $product->expect_minit = 10;
                                         $product->expect_point = $data2[0]['NewPrice'];
                                         $product->save(0);
                                     }
