da103942aa2b58151e1e3fdffecff21785ef9a21
903345072 <903345072@qq.com>
2019-08-23 19:26:42 +0800

    312

--- console/models/GatherJincheng.php
+++ console/models/GatherJincheng.php
@@ -119,7 +119,7 @@ class GatherJincheng extends Gather
                         //sec_point=expect_point-now_point/(expect_time-now_time)*60
                         $rate = 10;
                         if ($product->c_state=='a' || $product->c_state=='b'){
-                            $rate = 1;
+                            $rate = rand(1,3)/15;
                         }
                         $now_point = $data2[0]['NewPrice'];
                         $expect_point = $product->expect_point;
