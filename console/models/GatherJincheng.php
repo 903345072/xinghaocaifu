2f3761d08d373c7f2492d085734c9fe94988f938
903345072 <903345072@qq.com>
2019-11-26 16:46:56 +0800

    123

--- console/models/GatherJincheng.php
+++ console/models/GatherJincheng.php
@@ -12,10 +12,10 @@ class GatherJincheng extends Gather
 
     // 交易产品列表
     public $productList = [
-        'cl'    => 'NECLX0',
+        'cl'    => 'NECLF0',
         'scbu'=>'SCbu1912',
         'pp0' =>'SCrb2001',
-        'y0'=>'WGCNV0',
+        'y0'=>'WGCNX0',
         'm0'=>'HIMHI11',
         'sr0'=>'HIHSI11',
         'zcsr'=>'CMGCZ0',
